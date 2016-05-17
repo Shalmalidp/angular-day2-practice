568871e94deb2ff996d1338f9459c69b636db6a6
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-05-17 11:34:01 -0400

    added config.js and registered it

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+function config(){
+  
+
+
+}
+
+config.$inject = [];
+
+export { config };
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import angular from 'angular';
+import 'angular-ui-router';
+
 import $ from 'jquery';
 
 let url = 'https://secret-forest-21470.herokuapp.com/collections/angularday2';
@@ -6,10 +8,14 @@ let url = 'https://secret-forest-21470.herokuapp.com/collections/angularday2';
 
 // CONTROLLERS
 import { HomeController } from './controllers/home.controller';
+import { config } from './config';
+
+
 
 //REGISTERING COMPONENTS
 angular
-  .module('app',[])
+  .module('app',['ui.router'])
   .constant('URL', url)
+  .config(config)
   .controller('HomeController',HomeController)
 ;
