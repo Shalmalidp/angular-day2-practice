bf355921124a25b601495aca29e561014eb4d37a
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-05-17 10:49:58 -0400

    added .post to our controller

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -1,11 +1,21 @@
-function HomeController($scope){
+function HomeController($scope, URL, $http){
 
   $scope.name = 'Shalmali Dalvi Padgaokar';
+  $scope.addPet = function(petObj){
+   
+    $http.post(URL,petObj).then(function(res){
+      console.log(res);
+      $scope.pet={};
+    });
+
+  };
+
   
+
  
 }
 
 
-HomeController.$inject=['$scope'];
+HomeController.$inject=['$scope','URL','$http'];
 
 export {HomeController};
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 import angular from 'angular';
 import $ from 'jquery';
 
-// console.log($);
-// console.log(angular);
+let url = 'https://secret-forest-21470.herokuapp.com/collections/angularday2';
 
+
+// CONTROLLERS
 import { HomeController } from './controllers/home.controller';
 
-
-
+//REGISTERING COMPONENTS
 angular
   .module('app',[])
+  .constant('URL', url)
   .controller('HomeController',HomeController)
 ;
